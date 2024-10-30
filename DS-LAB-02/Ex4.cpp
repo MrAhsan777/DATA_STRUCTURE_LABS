d61e458c1d7e766f315c18906ae0fa7e9140d316
Muhammad Ahsan <150921311+MrAhsan777@users.noreply.github.com>
2024-10-30 20:41:17 +0500

    create

---
+++ DS-LAB-02/Ex4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void add(int nums[], int &size, int loc, int item) {
+    for (int i = size; i > loc; i--) {
+        nums[i] = nums[i - 1];
+    }
+    nums[loc] = item;
+    size++;
+}
+
+int main() {
+    int nums[10] = {1, 2, 3, 4, 5, 7};
+    int size = 6;
+    int item = 6;
+    int loc = 5; // index
+
+    add(nums, size, loc, item);
+
+    for (int i = 0; i < size; i++) cout << nums[i] << " ";
+}
